1b567e40feb90af92240181be4abd7103aee9c7f
BannyT <tumusiimebarnabus@gmail.com>
2020-03-01 22:27:30 +0300

    worked on chart js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ,ElementRef} from '@angular/core';
+import { WeatherServiceService } from './services/weather-service.service';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'BuddeAPP';
+export class AppComponent implements OnInit {
+   constructor( public weath:WeatherServiceService, public elementRef: ElementRef){}
+   temp_max:any;
+   temp_min:any;
+   lineChart= [];
+   
+   weatherDates = [];
+   ngOnInit(){
+    this.weath.dailyForecast().subscribe( res =>{
+      console.log(res);
+
+      this.temp_max = res['list'].map(res => res.main.temp_max);
+       this.temp_min =res['list'].map(res => res.main.temp_min);
+      let alldates =res['list'].map(res => res.dt)
+      
+      console.log(this.temp_max); 
+   alldates.forEach((res) => {
+    let jsdate = new Date(res * 1000)
+    this.weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric' }))
+})
+    })
+    let htmlRef = this.elementRef.nativeElement.querySelector(`#lineChart`);
+   this.lineChart = new Chart(htmlRef, {
+      type: 'line',
+      data: {
+        labels: this.weatherDates,
+        datasets: [
+          { 
+            data: this.temp_max,
+            borderColor: "#3cba9f",
+            fill: false
+          },
+          { 
+            data: this.temp_min,
+            borderColor: "#ffcc00",
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+
+
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherServiceService } from './services/weather-service.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [WeatherServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
